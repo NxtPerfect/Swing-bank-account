6714ae0f57651352b556bef13dc7312c86c3f86e
NxtPerfect <alakaxan@protonmail.com>
2023-10-24 17:41:34 +0200

    Working on centering labels

--- ui.java
+++ ui.java
@@ -1,5 +1,6 @@
 import java.awt.Frame;
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 public class Ui extends JFrame {
   public static void main(String[] args) {
@@ -9,18 +10,30 @@ public class Ui extends JFrame {
   public Ui() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(800, 600);
-    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    setVisible(true);
+    setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
     String balance = "100,000$";
 
-    JLabel welcome = new JLabel("Welcome to Bank Management System.");
-    JLabel yourBalance = new JLabel("Your bank balance is");
-    JLabel balanceLabel = new JLabel(balance);
+    JPanel panel = new JPanel();
 
-    add(welcome);
-    add(yourBalance);
-    add(balanceLabel);
+    JLabel welcome = new JLabel("Welcome to Bank Management System.", JLabel.CENTER);
+    welcome.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+    JLabel yourBalance = new JLabel("Your bank balance is", JLabel.CENTER);
+    yourBalance.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+    JLabel balanceLabel = new JLabel(balance, JLabel.CENTER);
+    balanceLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+    panel.add(Box.createVerticalGlue());
+    panel.add(welcome);
+    panel.add(Box.createVerticalGlue());
+    panel.add(yourBalance);
+    panel.add(Box.createVerticalGlue());
+    panel.add(balanceLabel);
+    panel.add(Box.createVerticalGlue());
+    panel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+    panel.setSize(700, 550);
+    getContentPane().add(panel, JPanel.CENTER_ALIGNMENT);
     pack();
-    setVisible(true);
   }
 }
